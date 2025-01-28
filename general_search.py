af61e12edb76804178d416cf99da0be447a39133
Sreeja Pagireddy <sreeja@karibu.ai>
2025-01-28 11:50:11 -0800

    added what i have rn

--- general_search.py
+++ general_search.py
@@ -1,13 +1,30 @@
 #lets first create a function
-import numpy as np
 class Node:    
     def __init__(self,state):
         self.state = state
         self.children = []
         self.parent = []
-def initial_input():
+
+def initial_input_puzzle():
     print("Lets start playing the 8 puzzle. Please enter valid 8-puzzle inputs in each row with a space when asked.")
-    entry_message = input("Please Enter the first Row, make sure to include space between.")
+    print("Please enter 9 numbers where one of them is a 0 to represent a blank space ")
+    matrix=[]
+    #cited from https://www.geeksforgeeks.org/python-matrix/ to build the matrix that is needed so I can display it
+    #going through the row which has 3
+    for row in range(3):
+        first = [] #creating an array
+        for column in range(3): #
+            first.append(int(input()))
+        matrix.append(first)
+    print("Here is your 8 puzzle that you generated")
+    for row in range(3):
+        for column in range(3):
+            print(matrix[row][column], end=" " )
+        print()
+    #Now we want the user to pick the heurisitc they are going to use to solve the algorithm
+    Heuristic = input("Please pick the heuristic you want to use to solve the algorithm. 1. Uniform Cost Search, 2. The Manhattan distance, 3.The Misplaced Tile: ")
+    if(Heuristic==1):
+        general_search(matrix,0) #the problem here would be the matrix
 
 def queue_make_node(initial_state):
     #create a new quene and node
@@ -30,6 +47,7 @@ def general_search(problem,QUEUEING_FUNCTION, target):
 
 def main():
     #assuming we
-    initial_input()
+    initial_input_puzzle()
+
 
 main()
