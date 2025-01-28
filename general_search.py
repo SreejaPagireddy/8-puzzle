c44e992c1d48d08bfe725590a74905e83c596fb3
Sreeja Pagireddy <sreeja@karibu.ai>
2025-01-28 13:29:16 -0800

    adding wat i have right now

--- general_search.py
+++ general_search.py
@@ -2,8 +2,39 @@
 class Node:    
     def __init__(self,state):
         self.state = state
-        self.children = []
         self.parent = []
+    def children(self):
+        #now creating the children of the matrixes
+        return_children = []
+        for i in range(4):
+            matrix=[]
+            for row in range(3):
+                first = [] #creating an array
+                for column in range(3): #
+                    first.append(self.state[row][column])
+                matrix.append(first)
+            for row in range(3):
+                for column in range(3):
+                    if(matrix[row][column]==0):
+                        if i==0:
+                            if(column < len(matrix)-1):
+                                matrix[row][column], matrix[row][ column + 1] =  matrix[row][column + 1], matrix[row][column]
+                                return_children.append(matrix)
+                        if i==1:
+                            if(column < len(matrix)-1):
+                                matrix[row][column], matrix[row][column - 1] =  matrix[row][column - 1], matrix[row][column]
+                                return_children.append(matrix)
+                        if i==2:
+                            if(column < len(matrix)-1):
+                                matrix[row][column], matrix[row+1][column] =  matrix[row+1][column], matrix[row][column]
+                                return_children.append(matrix)
+                        if i==3:
+                            if(column < len(matrix)-1):
+                                matrix[row][column], matrix[row-1][column] =  matrix[row-1][column], matrix[row][column]
+                                return_children.append(matrix)
+        
+        return return_children
+
 
 def initial_input_puzzle():
     print("Lets start playing the 8 puzzle. Please enter valid 8-puzzle inputs in each row with a space when asked.")
@@ -21,10 +52,7 @@ def initial_input_puzzle():
         for column in range(3):
             print(matrix[row][column], end=" " )
         print()
-    #Now we want the user to pick the heurisitc they are going to use to solve the algorithm
-    Heuristic = input("Please pick the heuristic you want to use to solve the algorithm. 1. Uniform Cost Search, 2. The Manhattan distance, 3.The Misplaced Tile: ")
-    if(Heuristic==1):
-        general_search(matrix,0) #the problem here would be the matrix
+    return matrix
 
 def queue_make_node(initial_state):
     #create a new quene and node
@@ -33,21 +61,30 @@ def queue_make_node(initial_state):
     queue.append(new_node)
     return queue
 
-def general_search(problem,QUEUEING_FUNCTION, target):
-    nodes = queue_make_node(problem.initial_state())
+def general_search(problem, target):
+    #make
+    repeat = set()
+    nodes = queue_make_node(problem)
     #check if the whole quene is empty
-    while (not nodes.empty()):
-        if(curNode==target): #this is how we are checking if its a goal state
-            return curNode
+    while (len(nodes)!=0):
         curNode = nodes.pop(0) #remove the first element
-        for child in curNode.children: #simmilar to expanding
+        #check if the state is repeated, pop it
+        if(curNode.state == target): #this is how we are checking if its a goal state
+            return curNode
+        for child in curNode.children(): #simmilar to expanding
             child.parent = curNode
             nodes.append(child)
     return "Failure"
 
 def main():
     #assuming we
-    initial_input_puzzle()
+    matrix = initial_input_puzzle()
+    #Now we want the user to pick the heurisitc they are going to use to solve the algorithm
+    Heuristic = input("Please pick the heuristic you want to use to solve the algorithm. 1. Uniform Cost Search, 2. The Manhattan distance, 3.The Misplaced Tile: ")
+    target = [[1, 2, 3], [4, 0, 5], [6, 7, 8]] #sample target
+    if(Heuristic==1):
+        general_search(matrix, target) #the problem here would be the matrix
+    print(matrix)
 
 
 main()
