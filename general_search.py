d8986ddb0cd70dd1ab78687a9fc478e518c2d6ee
Sreeja Pagireddy <sreeja@karibu.ai>
2025-01-28 14:41:25 -0800

    adding wat i have right now

--- general_search.py
+++ general_search.py
@@ -3,6 +3,10 @@ class Node:
     def __init__(self,state):
         self.state = state
         self.parent = []
+    #this is how we print a node
+    def print(self):
+        print(self.state)
+
     def children(self):
         #now creating the children of the matrixes
         return_children = []
@@ -13,29 +17,36 @@ class Node:
                 for column in range(3): #
                     first.append(self.state[row][column])
                 matrix.append(first)
+            done  = False
             for row in range(3):
                 for column in range(3):
                     if(matrix[row][column]==0):
+                        done = True
                         if i==0:
                             if(column < len(matrix)-1):
                                 matrix[row][column], matrix[row][ column + 1] =  matrix[row][column + 1], matrix[row][column]
                                 return_children.append(matrix)
-                        if i==1:
+                        elif i==1:
                             if(column >= 1):
                                 matrix[row][column], matrix[row][column - 1] =  matrix[row][column - 1], matrix[row][column]
                                 return_children.append(matrix)
-                        if i==2:
+                        elif i==2:
                             if(row + 1<3):
                                 matrix[row][column], matrix[row+1][column] =  matrix[row+1][column], matrix[row][column]
                                 return_children.append(matrix)
-                        if i==3:
+                        elif i==3:
                             if(row - 1>=0):
                                 matrix[row][column], matrix[row-1][column] =  matrix[row-1][column], matrix[row][column]
                                 return_children.append(matrix)
+                        #to avoid multiple manipulations
+                        if done:
+                            break
+                    if done:
+                        break
             #Now we want to create all the matrixes to node because we want to store the nodes
-            for row in range(len(return_children)):
-                create_node = Node(return_children[row])
-                return_children[row] = create_node
+        for row in range(len(return_children)):
+            create_node = Node(return_children[row])
+            return_children[row] = create_node
 
         return return_children
 
@@ -66,7 +77,6 @@ def queue_make_node(initial_state):
     return queue
 
 def general_search(problem, target):
-    print("general_search")
     #make
     repeat = set()
     nodes = queue_make_node(problem)
@@ -79,7 +89,7 @@ def general_search(problem, target):
             return curNode
         for child in curNode.children(): #simmilar to expanding
             child.parent = curNode
-            if(curNode not in repeat):
+            if(not child in repeat):
                 nodes.append(child) #appending children
     return "Failure"
 
@@ -88,10 +98,11 @@ def main():
     matrix = initial_input_puzzle()
     #Now we want the user to pick the heurisitc they are going to use to solve the algorithm
     Heuristic = input("Please pick the heuristic you want to use to solve the algorithm. 1. Uniform Cost Search, 2. The Manhattan distance, 3.The Misplaced Tile: ")
-    target = [[1, 2, 3], [4, 0, 5], [6, 7, 8]] #sample target
+    target = [[1, 2, 3], [4, 5, 0], [6, 7, 8]] #sample target
     if(Heuristic=="1"):
+        #this is a node
         result = (general_search(matrix, target)) #the problem here would be the matrix
-        print(result)
+        result.print() # we want to print from the class
 
 
 main()
